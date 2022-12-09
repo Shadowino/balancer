7bedc52949297b75a78dd4dabc750b2152539243
shadowino <savlovichdenis@gmail.com>
2022-12-09 11:11:25 +0300

    add ver10

---
+++ source/parser-10/LL.ino
@@ -0,0 +1,66 @@
+class message {
+  public:
+    // packet struct
+    // 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10
+    // 50 xx xx AX AX AY AY AZ AZ CR CR 
+    uint8_t type;
+    uint8_t data[20];
+    uint8_t len;
+    bool mready;
+    void add(uint8_t d) {
+      data[len] = d;
+      len++;
+    }
+    uint8_t check() {
+      if (data[0] == 0x50 and data[2] == 0x06) type = 2;
+      else if (data[0] == 0x50 and data[2] == 0x00) type = 1;
+      else if (data[0] == 0x55) type = 3;
+      else type = 0;
+      return type;
+    }
+
+    int16_t getY() {
+      if (type == 2) {
+        return (data[6] | (data[5] << 8));
+      } else {
+        return (data[6] | (data[5] << 8));
+      }
+
+    }
+
+    int16_t getX() {
+      if (type == 2) {
+        return (data[4] | (data[3] << 8));
+      } else {
+        return (data[4] | (data[3] << 8));
+      }
+
+    }
+
+};
+
+class motion {
+  public:
+    uint8_t STP;
+    uint8_t DIR;
+    uint8_t ENA;
+    uint32_t dt;
+    uint32_t stpdt;
+    uint32_t stpdel;
+    void ini(){
+      
+    }
+    void check(){
+      dt = micros();
+      if (dt - stpdt > stpdel){
+        stpdt = dt;
+        if(PINB & (1<<STP)){
+          PBoff(STP);
+        }else{
+          PBon(STP);
+        }
+      }
+    }
+    
+    
+};
--- source/parser-10/parser-10.ino
+++ source/parser-10/parser-10.ino
@@ -1,3 +1,4 @@
+#include <SoftwareSerial.h>
 
 #define PBon(x) PORTB |= (1 << x);
 #define PBoff(x) PORTB &= ~(1 << x);
@@ -6,22 +7,58 @@
 
 // PINnum = BITnumber + 8
 // BITnumber = PINnumber - 8
-// пины(контакты) моторов указываються через BITnumber 
+// пины(контакты) моторов указываються через BITnumber
 // и находяться на порту B
 #define MXDIR 0 //8
 #define MXSTP 1 //9
 #define MXENA 2 //10
 
-#define MXENA 3 //11
-#define MXSTP 4 //12
-#define MXDIR 5 //13
+#define MYENA 3 //11
+#define MYSTP 4 //12
+#define MYDIR 5 //13
+
+
+#define STPCYCLEY 8600
+#define TOSTEPY (65536/STPCYCLEY)
+
+#define SSENA true
+
+SoftwareSerial debug(2, 3);
+
+void movtest();
 
 void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  debug.begin(115200);
+  debug.setTimeout(1);
+  debug.println("STEEL @LIVE");
+  // DDRx |= (1 << y); => PinMode(y, OUTPUT);
+  DDRD |= (1 << 4); //
+  DDRD |= (1 << 5);
+  DDRD |= (1 << 6);
+  DDRD |= (1 << 7);
 
-
-}
-
-void loop() {
-
-
+  DDRB |= (1 << 0);
+  DDRB |= (1 << 1);
+  DDRB |= (1 << 2);
+  DDRB |= (1 << 3);
+  DDRB |= (1 << 4);
+  DDRB |= (1 << 5);
+  PDoff(4)
+  PDoff(5)
+  PDoff(6)
+  PDoff(7)
+  PBoff(MXDIR);
+  PBoff(MXSTP);
+  PBoff(MXENA);
+  PBoff(MYENA);
+  PBoff(MYSTP);
+  PBoff(MYDIR);
+  if (SSENA) {
+    debug.print("HS:");
+    debug.print(((Serial) ? "true" : "false"));
+    debug.print(" SS:");
+    debug.println(((debug) ? "true" : "false"));
+  }
 }
